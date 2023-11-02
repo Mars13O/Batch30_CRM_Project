1caac878393957433c9242cba33bbf14af439bf8
Nemanja9513 <nempejovic@gmail.com>
2023-11-02 10:26:09 -0400

    Created SendMessages Step Definition Class

---
+++ src/test/java/com/Batch30_Sprint2_Implementation/step_definitions/B30G18_130_SendMessages_StepDefinitions.java
@@ -0,0 +1,102 @@
+package com.Batch30_Sprint2_Implementation.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class B30G18_130_SendMessages_StepDefinitions {
+
+    @Given("User is logged in to the CRM HomePage")
+    public void user_is_logged_in_to_the_crm_home_page() {
+
+    }
+    @When("User clicks on Message Tab")
+    public void user_clicks_on_message_tab() {
+
+    }
+    @When("User types in a {string} in message input box")
+    public void user_types_in_a_in_message_input_box(String string) {
+
+    }
+    @When("User does not leave the recipient box blank by deleting the default option")
+    public void user_does_not_leave_the_recipient_box_blank_by_deleting_the_default_option() {
+
+    }
+    @Then("User should be able to send a message by filling the mandatory fields")
+    public void user_should_be_able_to_send_a_message_by_filling_the_mandatory_fields() {
+
+    }
+
+
+    @Given("User is logged in to the CRM HomePage")
+    public void user_is_logged_in_to_the_crm_home_page() {
+
+    }
+    @Given("User clicks on Message Tab")
+    public void user_clicks_on_message_tab() {
+
+    }
+    @When("User leaves message content box empty")
+    public void user_leaves_message_content_box_empty() {
+
+    }
+    @When("User clicks Send button")
+    public void user_clicks_send_button() {
+
+    }
+    @Then("User should see an error message {string}")
+    public void user_should_see_an_error_message(String string) {
+
+    }
+
+
+    @Given("User is logged in to the CRM HomePage")
+    public void user_is_logged_in_to_the_crm_home_page() {
+
+    }
+    @Given("User clicks on Message Tab")
+    public void user_clicks_on_message_tab() {
+
+    }
+    @When("User leaves recipient box empty by deleting {string} button")
+    public void user_leaves_recipient_box_empty_by_deleting_button(String string) {
+
+    }
+    @When("User clicks Send button")
+    public void user_clicks_send_button() {
+
+    }
+    @Then("User should see an error message {string}")
+    public void user_should_see_an_error_message(String string) {
+
+    }
+
+
+    @Given("User is logged in to the CRM HomePage")
+    public void user_is_logged_in_to_the_crm_home_page() {
+
+    }
+    @When("User clicks on Message Tab")
+    public void user_clicks_on_message_tab() {
+
+    }
+    @Then("User should verify that the message delivery recipient should be All employees by default")
+    public void user_should_verify_that_the_message_delivery_recipient_should_be_all_employees_by_default() {
+
+    }
+
+
+    @Given("User is logged in to the CRM HomePage")
+    public void user_is_logged_in_to_the_crm_home_page() {
+
+    }
+    @When("User clicks on Message Tab")
+    public void user_clicks_on_message_tab() {
+
+    }
+    @Then("User should be able to cancel sending message at any time before sending")
+    public void user_should_be_able_to_cancel_sending_message_at_any_time_before_sending() {
+
+    }
+
+}
