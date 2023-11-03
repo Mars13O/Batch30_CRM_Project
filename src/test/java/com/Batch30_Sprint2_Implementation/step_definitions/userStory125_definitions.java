a09724a4f8c96284d63cb88e6945fa36f85bdfdb
sbai0 <sbai06@hotmail.com>
2023-11-03 19:29:25 -0400

    I created new definition class

--- src/test/java/com/Batch30_Sprint2_Implementation/step_definitions/userStory125_definitions.java
+++ src/test/java/com/Batch30_Sprint2_Implementation/step_definitions/userStory125_definitions.java
@@ -1,18 +1,12 @@
 package com.Batch30_Sprint2_Implementation.step_definitions;
 
-import com.Batch30_Sprint2_Implementation.pages.LoginPage;
-import com.Batch30_Sprint2_Implementation.utilities.ConfigurationReader;
 import com.Batch30_Sprint2_Implementation.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.junit.Assert;
-import org.openqa.selenium.WebDriver;
-
-import static com.Batch30_Sprint2_Implementation.utilities.Driver.getDriver;
 
 public class userStory125_definitions {
     @Given("the user is on the Company page")
-    public void the_user_is_on_the_company_page() {
+    public void the_user_is_on_the_company_page() {Driver.getDriver("https://login1.nextbasecrm.com/about/");
 
     }
     @Then("the user should see {string} module")
